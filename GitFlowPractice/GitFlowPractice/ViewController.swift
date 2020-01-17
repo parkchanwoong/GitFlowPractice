7cdf3c40b68ffa1154cf3a7d4d7b361e7300e37a
박찬웅 <foodi1121@gmail.com>
2020-01-17 13:58:10 +0900

    [FIX] 긴급버그 Fix

--- GitFlowPractice/GitFlowPractice/ViewController.swift
+++ GitFlowPractice/GitFlowPractice/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print("버그수정완료!")
         // Do any additional setup after loading the view.
     }
 

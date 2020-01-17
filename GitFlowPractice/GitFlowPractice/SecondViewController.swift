7271c6d7af373c55ceb0a8229e29b34f76eb444e
박찬웅 <foodi1121@gmail.com>
2020-01-17 15:49:56 +0900

    [Add] ViewController extension 추가 showSimpleAlert(message)

--- GitFlowPractice/GitFlowPractice/SecondViewController.swift
+++ GitFlowPractice/GitFlowPractice/SecondViewController.swift
@@ -14,8 +14,23 @@ class SecondViewController: UIViewController {
         super.viewDidLoad()
         
         print("SecondViewController ::",#function)
-        print("릴리즈에서 추가한 단순 버그 수정)
+        print("릴리즈에서 추가한 단순 버그 수정")
 
     }
 
+}
+
+extension ViewController {
+    func showSimpleAlert(message: String) {
+        let alertController = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "확인", style: .default, handler: nil)
+        let cancelAction = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+        let destructive = UIAlertAction(title: "파괴적인행동", style: .destructive, handler: nil)
+        
+        alertController.addAction(okAction)
+        alertController.addAction(cancelAction)
+        alertController.addAction(destructive)
+        
+        present(alertController, animated: true, completion: nil)
+    }
 }
